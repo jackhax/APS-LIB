2a59e08eeb03af32b86ec48234f7205e659cef44
Adnan Jakati <41838363+jackhax@users.noreply.github.com>
2021-06-24 13:27:24 +0530

    Create ReverseLinkedList.cpp

---
+++ functions/ReverseLinkedList.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+ 
+typedef struct node
+{
+  int val;
+  struct node* next;
+}node;
+ 
+node* head=NULL;
+ 
+int count(node* head) // code to count the no. of nodes
+{
+  node* p=head;
+  int k=1;
+  while(p!=NULL)
+  {
+    p=p->next;
+    k++;
+  }
+  return k;
+}
+ 
+node *ll_reverse(node* head)
+{
+  node* p=head;
+  long int i=count(head),j=1;
+  long int arr[i];
+  while(i && p!=NULL)
+  {
+    arr[j++]=p->val;
+    p=p->next;
+    i--;
+  }
+  j--;
+  while(j) // loop will break as soon as j=0
+  {
+    cout<<arr[j--]<<" ";
+  }
+   
+  return head;
+}
+ 
+node* insert_end(node* head,int data)  //code to insert at end of ll
+{
+  node* q=head,*p=(node*)malloc(sizeof(node));
+  p->val=data;
+  while(q->next!=NULL)
+  {
+    q=q->next;
+  }
+  q->next=p;
+  p->next=NULL;
+  return head;
+}
+ 
+node *create_ll(node* head,int data)
+{
+  node* p=(node*)malloc(sizeof(node));
+  p->val=data;
+  if(head==NULL)
+  {
+    head=p;
+    p->next=NULL;
+    return head;
+  }
+  else
+  {
+    head=insert_end(head,data);
+    return head;
+  }
+}
+int main()
+{
+  int i=5,j=1; 
+  while(i--)
+  {
+    head=create_ll(head,j++);
+  }
+  head=ll_reverse(head); 
+    return 0;
+}
